ab754b56aff272c3544f9139f8a4c73f7007b0d8
emilyagras <emily@gras.org>
2017-04-12 20:09:52 +0200

    still mostly unintelligible game of life

--- js-display/scripts/init.js
+++ js-display/scripts/init.js
@@ -1,16 +1,17 @@
-// First get the window height and width
+// get the window height and width
 var winHeight = jQuery(window).height();
 var winWidth = jQuery(window).width();
 var windowHash = jQuery(location).attr('hash');
 
-var xGridWidth = (0.75 * winWidth); // Get 80% dimension
+// Set Grid Width/Height to a little less than the window size
+var xGridWidth = (0.70 * winWidth); // Get 70% dimension
 var yGridWidth = (0.70 * winHeight); // Get 70% dimension
 
 var xCells = 50;
-var yCells = 24;
+var yCells = 50;
 
-var xcellWidth = d3.round(xGridWidth / xCells);
 var ycellWidth = d3.round(yGridWidth / yCells) - 1;
+var xcellWidth = ycellWidth;
 
 var xWin = (xCells) * (xcellWidth);
 var yWin = (yCells) * (ycellWidth);
