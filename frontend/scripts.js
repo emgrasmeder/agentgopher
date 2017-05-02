3627f2898a864e0e9a678bafebc8a737d8dbc514
emilyagras <emily@gras.org>
2017-05-02 22:57:59 +0200

    #3 enable square to disappear at my command

---
+++ frontend/scripts.js
@@ -0,0 +1,14 @@
+function hideToggle() {
+    console.log("hello world")
+    document.getElementById('box').style.visibility = "hidden";
+}
+
+function incrementValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+    if (value > 2) {
+        hideToggle();
+    }
+}
