965378a9171f2db3e979853583e47b231820cfe6
Emily Grasmeder <emily@gras.org>
2017-09-14 19:31:26 +0200

    A websocket handshake (#16)
    * Add golang websocket which accepts client from App.js

---
+++ golang-websocket/server.go
@@ -0,0 +1,49 @@
+// +build ignore
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var addr = flag.String("addr", "localhost:8080", "http service address")
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Handling connection from client...")
+	socket, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade error: ", err)
+		return
+	}
+	defer socket.Close()
+	for {
+		messageType, message, err := socket.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("Received message from client %s", message)
+		err = socket.WriteMessage(messageType, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
+
+func main() {
+	flag.Parse()
+	log.SetFlags(0)
+	http.HandleFunc("/echo", echo)
+	fmt.Println("Listening and serving at ", *addr)
+	log.Fatal(http.ListenAndServe(*addr, nil))
+}
